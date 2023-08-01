8e15ac40913f42de33b2859d83b20a4308ee3fe9
Matias Marinelli <matiasr86@hotmail.com>
2023-08-01 19:45:36 -0300

    Correccion de indentacion en general

--- logic.js
+++ logic.js
@@ -3,43 +3,42 @@ var top10 = [];
 getTop10();
 
 // Guardamos en variables los elementos del DOM que vamos a manipular
-var flotanteDiv = document.querySelector('.flotante');
-flotanteDiv.style.cursor = 'default'
+var flotanteDiv = document.querySelector(".flotante");
+flotanteDiv.style.cursor = "default";
 
-var buttonCloseModal = document.getElementById('close-modal');
-var scoreElement = document.getElementById('score');
-
+var buttonCloseModal = document.getElementById("close-modal");
+var scoreElement = document.getElementById("score");
 
 // Rojo es el numero 1
 // Azul es el numero 2
 // Amarillo es el numero 3
 // Verde es el numero 4
 
-var red = document.getElementById('red');
-var blue = document.getElementById('blue');
-var yellow = document.getElementById('yellow');
-var green = document.getElementById('green');
+var red = document.getElementById("red");
+var blue = document.getElementById("blue");
+var yellow = document.getElementById("yellow");
+var green = document.getElementById("green");
 
-var stateDiv = document.querySelector('.state');
+var stateDiv = document.querySelector(".state");
 var timeDiv = stateDiv.children[1];
-var titleTime = timeDiv.querySelector('h2');
-var modal = document.querySelector('.modal');
+var titleTime = timeDiv.querySelector("h2");
+var modal = document.querySelector(".modal");
 
 var levelDiv = stateDiv.children[0];
-var titleLevel = levelDiv.querySelector('h2');
+var titleLevel = levelDiv.querySelector("h2");
 
-// Eventos 
-var buttonPlay = document.getElementById('play');
-buttonPlay.addEventListener('click', start);
+// Eventos
+var buttonPlay = document.getElementById("play");
+buttonPlay.addEventListener("click", start);
 
-buttonCloseModal.addEventListener('click', closeModal);
+buttonCloseModal.addEventListener("click", closeModal);
 
-red.addEventListener('click', checkSecuencia);
-blue.addEventListener('click', checkSecuencia);
-yellow.addEventListener('click', checkSecuencia);
-green.addEventListener('click', checkSecuencia);
+red.addEventListener("click", checkSecuencia);
+blue.addEventListener("click", checkSecuencia);
+yellow.addEventListener("click", checkSecuencia);
+green.addEventListener("click", checkSecuencia);
 
-// Inicializamos las variables globales que necesitaremos 
+// Inicializamos las variables globales que necesitaremos
 
 var sequenceMain = [];
 var sequencePlayer = [];
@@ -55,29 +54,27 @@ var time = 3;
 var timeAcumulado = 3;
 var temp;
 var reset;
-var timeEnableplay= 0;
+var timeEnableplay = 0;
 var timeUtilizado = 0;
 
 // Funcion para obtener el top 10 de puntajes desde el localStorage
-function getTop10(){
+function getTop10() {
   /* localStorage.clear(); */
-  if(localStorage.length > 0){
-    for(var i = 0; i < 10; i++){
+  if (localStorage.length > 0) {
+    for (var i = 0; i < 10; i++) {
       var key = " " + i;
       var scoreJSON = localStorage.getItem(key);
       var objScore = JSON.parse(scoreJSON);
       top10.push(objScore);
     }
-
-  }else{
-    
-    for(var i = 0; i < 10; i++){
+  } else {
+    for (var i = 0; i < 10; i++) {
       var key = " " + i;
-      var scorePlayer ={
-        date: '0000-00-00',
-        player: 'Sin Registro',
+      var scorePlayer = {
+        date: "0000-00-00",
+        player: "Sin Registro",
         level: 0,
-        score: 0
+        score: 0,
       };
       var scoreJSON = JSON.stringify(scorePlayer);
       localStorage.setItem(key, scoreJSON);
@@ -86,20 +83,21 @@ function getTop10(){
   }
 }
 
-// Iniciamos el juego 
-function start(e){
+// Iniciamos el juego
+function start(e) {
   // Para evitar que recargue la pagina
   e.preventDefault();
-  if(nameVar){
-    if(newGame){
+  if (nameVar) {
+    if (newGame) {
       resetVars();
     }
-    buttonPlay.style.display = 'none';
+    buttonPlay.style.display = "none";
     play();
-  }else{
-    // si el jugador no coloca el nombre 
+  } else {
+    // si el jugador no coloca el nombre
     var spanNameNo = document.createElement(`span`);
-    spanNameNo.textContent = '❌ Debe escribir un nombre de al menos 3 caracteres';
+    spanNameNo.textContent =
+      "❌ Debe escribir un nombre de al menos 3 caracteres";
     spanNameNo.style.color = `red`;
     spanNameNo.style.fontSize = `11px`;
     spanNameNo.style.display = `block`;
@@ -109,13 +107,13 @@ function start(e){
 }
 
 // Funcion para resetear los valores de las variables al reiniciar el juego
-function resetVars(){
-  titleLevel.textContent = 'Nivel: 0';
+function resetVars() {
+  titleLevel.textContent = "Nivel: 0";
   sequenceMain = [];
   sequencePlayer = [];
   countColorOk = 0;
   score = 0;
-  level= 0;
+  level = 0;
   count = 0;
   state = false;
   newGame = false;
@@ -123,14 +121,14 @@ function resetVars(){
   timeAcumulado = 3;
   timeUtilizado = 0;
 
-  red.style.backgroundColor = 'rgb(100, 0, 0)';
-  red.style.border = '8px solid rgb(48, 46, 46)';
-  blue.style.backgroundColor = 'rgb(0, 0, 100)';
-  blue.style.border = '8px solid rgb(48, 46, 46)';
-  yellow.style.backgroundColor = 'rgb(204, 204, 0)';
-  yellow.style.border = '8px solid rgb(48, 46, 46)';
-  green.style.backgroundColor = 'rgb(0, 80, 0)';
-  green.style.border = '8px solid rgb(48, 46, 46)';
+  red.style.backgroundColor = "rgb(100, 0, 0)";
+  red.style.border = "8px solid rgb(48, 46, 46)";
+  blue.style.backgroundColor = "rgb(0, 0, 100)";
+  blue.style.border = "8px solid rgb(48, 46, 46)";
+  yellow.style.backgroundColor = "rgb(204, 204, 0)";
+  yellow.style.border = "8px solid rgb(48, 46, 46)";
+  green.style.backgroundColor = "rgb(0, 80, 0)";
+  green.style.border = "8px solid rgb(48, 46, 46)";
 }
 
 // Funcion para optener un valor random para incorporar a la secuencia principal
@@ -140,37 +138,37 @@ function getNumberRandom() {
 }
 
 // Iniciamos el juego
-function play(){
-  titleTime.textContent = 'Recordar...';
+function play() {
+  titleTime.textContent = "Recordar...";
   var numRan = getNumberRandom();
   sequenceMain.push(numRan);
-  
+
   // Ejecutamos la secuencia
-  for(var i = 0; i<= sequenceMain.length; i++){
-    var wait = (i * 1500);
+  for (var i = 0; i <= sequenceMain.length; i++) {
+    var wait = i * 1500;
     execute(sequenceMain[i], wait);
-    if(i == sequenceMain.length){
+    if (i == sequenceMain.length) {
     }
   }
   count = 0;
-  setTimeout(function(){
+  setTimeout(function () {
     timeAvailable();
-    state = true; 
+    state = true;
   }, wait);
 }
 
-// Temporizador 
-function timeAvailable(){
+// Temporizador
+function timeAvailable() {
   timeEnableplay = time;
-  titleTime.textContent = 'Tiempo: ' + timeEnableplay;
-  setTimeout(function(){
+  titleTime.textContent = "Tiempo: " + timeEnableplay;
+  setTimeout(function () {
     temp;
-  },1000);
-  temp = setInterval(function(){
+  }, 1000);
+  temp = setInterval(function () {
     timeEnableplay = timeEnableplay - 0.1;
-    titleTime.textContent = 'Tiempo: ' + timeEnableplay.toFixed(2);
-    if(timeEnableplay <= 0 && state){
-      titleTime.textContent = 'Tiempo: 0';
+    titleTime.textContent = "Tiempo: " + timeEnableplay.toFixed(2);
+    if (timeEnableplay <= 0 && state) {
+      titleTime.textContent = "Tiempo: 0";
       timeUtilizado = timeUtilizado + (time - timeEnableplay);
       youLose();
       return;
@@ -178,181 +176,178 @@ function timeAvailable(){
   }, 100);
 }
 
-// Se invoca a la ejecucion de la secuencia 
+// Se invoca a la ejecucion de la secuencia
 function execute(num, seg) {
-  setTimeout(function(){
+  setTimeout(function () {
     changeColor(num);
   }, seg);
 }
 
-// Funcion que cambia la intensidad de los colores 
-function changeColor(num){
+// Funcion que cambia la intensidad de los colores
+function changeColor(num) {
   switch (num) {
     case 1:
       // Código a ejecutar si opcion es igual a 1
-      red.style.backgroundColor = 'rgb(255, 0, 0)';
-      red.style.border = '4px solid rgb(255, 255, 255)';
-      resetTime = setTimeout(function(){
-        reset()
+      red.style.backgroundColor = "rgb(255, 0, 0)";
+      red.style.border = "4px solid rgb(255, 255, 255)";
+      resetTime = setTimeout(function () {
+        reset();
       }, 1000);
       break;
     case 2:
       // Código a ejecutar si opcion es igual a 2
-      blue.style.backgroundColor = 'rgb(0, 50, 255)';
-      blue.style.border = '4px solid rgb(255, 255, 255)';
-      resetTime = setTimeout(function(){
-      reset()
+      blue.style.backgroundColor = "rgb(0, 50, 255)";
+      blue.style.border = "4px solid rgb(255, 255, 255)";
+      resetTime = setTimeout(function () {
+        reset();
       }, 1000);
       break;
     case 3:
       // Código a ejecutar si opcion es igual a 3
-      yellow.style.backgroundColor = 'rgb(255, 255, 0)';
-      yellow.style.border = '4px solid rgb(255, 255, 255)';
-      resetTime = setTimeout(function(){
-      reset()
+      yellow.style.backgroundColor = "rgb(255, 255, 0)";
+      yellow.style.border = "4px solid rgb(255, 255, 255)";
+      resetTime = setTimeout(function () {
+        reset();
       }, 1000);
       break;
     case 4:
       // Código a ejecutar si opcion es igual a 4
-      green.style.backgroundColor = 'rgb(0, 255, 0)';
-      green.style.border = '4px solid rgb(255, 255, 255)';
-      resetTime = setTimeout(function(){
-      reset()
+      green.style.backgroundColor = "rgb(0, 255, 0)";
+      green.style.border = "4px solid rgb(255, 255, 255)";
+      resetTime = setTimeout(function () {
+        reset();
       }, 1000);
       break;
   }
 }
 
 // Funcion para reestablecer los colores originales
-function reset(){
-    red.style.backgroundColor = 'rgb(100, 0, 0)';
-    red.style.border = '8px solid rgb(48, 46, 46)';
-    blue.style.backgroundColor = 'rgb(0, 0, 100)';
-    blue.style.border = '8px solid rgb(48, 46, 46)';
-    yellow.style.backgroundColor = 'rgb(204, 204, 0)';
-    yellow.style.border = '8px solid rgb(48, 46, 46)';
-    green.style.backgroundColor = 'rgb(0, 80, 0)';
-    green.style.border = '8px solid rgb(48, 46, 46)';
+function reset() {
+  red.style.backgroundColor = "rgb(100, 0, 0)";
+  red.style.border = "8px solid rgb(48, 46, 46)";
+  blue.style.backgroundColor = "rgb(0, 0, 100)";
+  blue.style.border = "8px solid rgb(48, 46, 46)";
+  yellow.style.backgroundColor = "rgb(204, 204, 0)";
+  yellow.style.border = "8px solid rgb(48, 46, 46)";
+  green.style.backgroundColor = "rgb(0, 80, 0)";
+  green.style.border = "8px solid rgb(48, 46, 46)";
 }
 
 // Funcion para chequear si la secuencia que el jugador va ingresando es correcta
-function checkSecuencia(e){
+function checkSecuencia(e) {
   // Para evitar que recargue la pagina
   e.preventDefault();
-  if(!state){
-      
-  }else{
+  if (!state) {
+  } else {
     var divId = e.target.id;
     var color = 0;
-    if(divId == 'red'){
+    if (divId == "red") {
       color = 1;
-      red.style.backgroundColor = 'rgb(255, 0, 0)';
-      red.style.border = '4px solid rgb(255, 255, 255)';
-      setTimeout(function(){
+      red.style.backgroundColor = "rgb(255, 0, 0)";
+      red.style.border = "4px solid rgb(255, 255, 255)";
+      setTimeout(function () {
         reset();
-      },100);
-    };
-    if(divId == 'blue'){
+      }, 100);
+    }
+    if (divId == "blue") {
       color = 2;
-      blue.style.backgroundColor = 'rgb(0, 50, 255)';
-      blue.style.border = '4px solid rgb(255, 255, 255)';
-      setTimeout(function(){
-         reset();
-      },100);
-    };
-    if(divId == 'yellow'){
-      color = 3;
-      yellow.style.backgroundColor = 'rgb(255, 255, 0)';
-      yellow.style.border = '4px solid rgb(255, 255, 255)';
-      setTimeout(function(){
-         reset();
-      },100);
-    };
-    if(divId == 'green'){
-      color = 4;
-      green.style.backgroundColor = 'rgb(0, 255, 0)';
-      green.style.border = '4px solid rgb(255, 255, 255)';
-      setTimeout(function(){
+      blue.style.backgroundColor = "rgb(0, 50, 255)";
+      blue.style.border = "4px solid rgb(255, 255, 255)";
+      setTimeout(function () {
         reset();
-      },100);
-    }; 
+      }, 100);
+    }
+    if (divId == "yellow") {
+      color = 3;
+      yellow.style.backgroundColor = "rgb(255, 255, 0)";
+      yellow.style.border = "4px solid rgb(255, 255, 255)";
+      setTimeout(function () {
+        reset();
+      }, 100);
+    }
+    if (divId == "green") {
+      color = 4;
+      green.style.backgroundColor = "rgb(0, 255, 0)";
+      green.style.border = "4px solid rgb(255, 255, 255)";
+      setTimeout(function () {
+        reset();
+      }, 100);
+    }
 
-    if(sequenceMain[count] != color){
+    if (sequenceMain[count] != color) {
       youLose();
-    }else{
+    } else {
       count++;
       countColorOk++;
-      if (count == sequenceMain.length){
+      if (count == sequenceMain.length) {
         timeUtilizado = timeUtilizado + (time - timeEnableplay);
-        level +=1;
+        level += 1;
         state = false;
         clearTimeout(temp);
-        titleLevel.textContent = 'Nivel: ' + level;
+        titleLevel.textContent = "Nivel: " + level;
         time += 0.5;
         timeAcumulado += time;
-        setTimeout(function(){
+        setTimeout(function () {
           play();
-        },1000);
+        }, 1000);
       }
     }
   }
 }
-  
-function youLose(){
+
+function youLose() {
   clearInterval(temp);
   clearTimeout(reset);
-  score = countColorOk - (timeAcumulado - timeUtilizado)/100;
+  score = countColorOk - (timeAcumulado - timeUtilizado) / 100;
   var dateNow = new Date().toISOString().slice(0, 10);
-    
+
   var scoreJugador = {
-  date: dateNow,
-  player: namePlayer,
-  level: level,
-  score: score
+    date: dateNow,
+    player: namePlayer,
+    level: level,
+    score: score,
   };
-    
+
   // Comprobamos si el puntaje realizado debe ingresar al top ten y lo colocamos en el lugar del ranking correspondiente
-  for(var i = 0; i < 10; i++){
+  for (var i = 0; i < 10; i++) {
     top10ObjScore = top10[i];
-      
-    if(top10ObjScore.score < score){
+
+    if (top10ObjScore.score < score) {
       var bajaUnaPosicion = top10ObjScore;
       top10[i] = scoreJugador;
-  
+
       var posicion = i + 1;
-      while(posicion < 10 ){
-      var objReemplazado = top10[posicion];
-      top10[posicion] = bajaUnaPosicion;
-        
-      bajaUnaPosicion = objReemplazado;
-      posicion ++;
+      while (posicion < 10) {
+        var objReemplazado = top10[posicion];
+        top10[posicion] = bajaUnaPosicion;
+
+        bajaUnaPosicion = objReemplazado;
+        posicion++;
       }
-      
+
       break;
     }
   }
-  
-  setTimeout(function(){
-    red.style.backgroundColor = 'rgb(0, 0, 0)';
-    blue.style.backgroundColor = 'rgb(0, 0, 0)';
-    yellow.style.backgroundColor = 'rgb(0, 0, 0)';
-    green.style.backgroundColor = 'rgb(0, 0, 0)';
-  },200);
-  
-  
+
+  setTimeout(function () {
+    red.style.backgroundColor = "rgb(0, 0, 0)";
+    blue.style.backgroundColor = "rgb(0, 0, 0)";
+    yellow.style.backgroundColor = "rgb(0, 0, 0)";
+    green.style.backgroundColor = "rgb(0, 0, 0)";
+  }, 200);
+
   state = false;
   newGame = true;
-  modal.style.display = 'block';
-  scoreElement.textContent = 'Score: ' + score.toFixed(2);
-  
+  modal.style.display = "block";
+  scoreElement.textContent = "Score: " + score.toFixed(2);
+
   // Actualizamos el localStorage
   updateTop10();
-  
 }
 
 // Funcion para actualizar el localStorage con los mejores 10 score
-function updateTop10(){
-  for(var i = 0; i < 10; i++){
+function updateTop10() {
+  for (var i = 0; i < 10; i++) {
     var score = top10[i];
     var key = " " + i;
     var scoreJSON = JSON.stringify(score);
@@ -360,70 +355,56 @@ function updateTop10(){
   }
 }
 
-
-function closeModal(){
-  buttonCloseModal.style.display = 'none';
-  buttonPlay.style.display = 'block';
-  buttonPlay.textContent = 'Reiniciar';
+function closeModal() {
+  buttonCloseModal.style.display = "none";
+  buttonPlay.style.display = "block";
+  buttonPlay.textContent = "Reiniciar";
 }
-
 
 // Validaciones
 
 // Validar el nombre
-var inputName = document.getElementById('nombre');
+var inputName = document.getElementById("nombre");
 var divName = inputName.parentNode;
 
 // Eventos
-inputName.addEventListener('blur', validName)
-inputName.addEventListener('focus', clearMessage)
+inputName.addEventListener("blur", validName);
+inputName.addEventListener("focus", clearMessage);
 
-function validName(e){
+function validName(e) {
   var textoIngresado = e.target.value;
-  if(textoIngresado != ""){
-    
-    if(textoIngresado.length < 3){
+  if (textoIngresado != "") {
+    if (textoIngresado.length < 3) {
       var spanNameNo = document.createElement(`span`);
-      spanNameNo.textContent = '❌ Debe escribir un nombre de al menos 3 caracteres';
+      spanNameNo.textContent =
+        "❌ Debe escribir un nombre de al menos 3 caracteres";
       spanNameNo.style.color = `red`;
       spanNameNo.style.fontSize = `11px`;
       spanNameNo.style.display = `block`;
       divName.insertBefore(spanNameNo, divName.children[2]);
       nameVar = false;
-    }
-    else{
+    } else {
       namePlayer = inputName.value;
-      var spanNameOk = document.createElement('span');
-      spanNameOk.textContent = '✅ Perfecto, a jugar!';
-      spanNameOk.style.color = 'green';
-      spanNameOk.style.fontSize = '12px';
-      spanNameOk.style.display = 'block';
+      var spanNameOk = document.createElement("span");
+      spanNameOk.textContent = "✅ Perfecto, a jugar!";
+      spanNameOk.style.color = "green";
+      spanNameOk.style.fontSize = "12px";
+      spanNameOk.style.display = "block";
       divName.insertBefore(spanNameOk, divName.children[2]);
       nameVar = true;
     }
-  }
-  else{
+  } else {
     nameVar = false;
   }
 }
 
-
 // Funcion para limpiar todos los mensajes de validación una vez el usuario vuelve a editar el input
 
-function clearMessage(e){
+function clearMessage(e) {
   var elementEvent = e.target;
-  var divParenElement = elementEvent.parentNode; 
+  var divParenElement = elementEvent.parentNode;
   var span = divParenElement.children[2];
-  if(span.tagName === `SPAN`){
+  if (span.tagName === `SPAN`) {
     divParenElement.removeChild(span);
   }
 }
-
-
-
-
-
-
-
-
-
--- ranking.js
+++ ranking.js
@@ -1,13 +1,12 @@
 // Obtenemos una referencia a la tabla
-var table = document.getElementById('my-table');
-var bodyTable = document.getElementById('body');
+var table = document.getElementById("my-table");
+var bodyTable = document.getElementById("body");
 var top10 = [];
 getTop10();
 
-
 // Funcion para obtener el top 10 de puntajes desde el localStorage
-function getTop10(){
-  for(var i = 0; i < 10; i++){
+function getTop10() {
+  for (var i = 0; i < 10; i++) {
     var key = " " + i;
     var scoreJSON = localStorage.getItem(key);
     var objScore = JSON.parse(scoreJSON);
@@ -17,27 +16,27 @@ function getTop10(){
 }
 
 // Creamos una función para agregar una fila a la tabla
-function addRow(fecha, jugador, nivel,  score) {
-  var row = document.createElement('tr');
+function addRow(fecha, jugador, nivel, score) {
+  var row = document.createElement("tr");
 
-  var celda1 = document.createElement('td');
+  var celda1 = document.createElement("td");
   celda1.textContent = fecha;
-  celda1.style.textAlign = 'center'
+  celda1.style.textAlign = "center";
   row.appendChild(celda1);
 
-  var celda2 = document.createElement('td');
+  var celda2 = document.createElement("td");
   celda2.textContent = jugador;
   row.appendChild(celda2);
 
-  var celda3 = document.createElement('td');
+  var celda3 = document.createElement("td");
   celda3.textContent = nivel;
-  celda3.style.textAlign = 'center'
+  celda3.style.textAlign = "center";
   row.appendChild(celda3);
 
-  var celda4 = document.createElement('td');
+  var celda4 = document.createElement("td");
   celda4.textContent = score.toFixed(2);
-  celda4.style.textAlign = 'center'
-  
+  celda4.style.textAlign = "center";
+
   row.appendChild(celda4);
   bodyTable.appendChild(row);
 }
